b34eae35f48ccb83de56e0902f542455552204b2
D0745662 <56393498+D0745662@users.noreply.github.com>
2021-10-23 15:36:30 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ Created on Wed Jun  2 21:16:35 2021
 @author: Ivan
 版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
 Line Bot聊天機器人
-第四章 選單功能
-快速回復QuickReply
+第三章 互動回傳功能
+推播push_message與回覆reply_message
 """
 #載入LineBot所需要的套件
 from flask import Flask, request, abort
@@ -18,6 +18,7 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 import re
+import time
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
@@ -26,6 +27,7 @@ line_bot_api = LineBotApi('F49hdXd2BxZ8DzznS+0I7GUhQYGI6OT97CvHWRihzS/1z/pcu9tTF
 handler = WebhookHandler('bca5248a9c30651b6730179bdd37515b')
 
 line_bot_api.push_message('U6ec9a01882a0c90ceaa97f7c7ae90ab8', TextSendMessage(text='你可以開始了'))
+yourID = 'U6ec9a01882a0c90ceaa97f7c7ae90ab8'
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -50,22 +52,39 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = text=event.message.text
-    if re.match('告訴我秘密',message):
+    if re.match('game',message):
         flex_message = TextSendMessage(text='以下有雷，請小心',
                                quick_reply=QuickReply(items=[
                                    QuickReplyButton(action=MessageAction(label="按我", text="按！")),
                                    QuickReplyButton(action=MessageAction(label="按我", text="按！")),
                                    QuickReplyButton(action=MessageAction(label="按我", text="按！")),
-                                   QuickReplyButton(action=MessageAction(label="別按我", text="你按屁喔！爆炸了拉！！")),
+                                   QuickReplyButton(action=MessageAction(label="別按我", text="爆炸了！！")),
                                    QuickReplyButton(action=MessageAction(label="按我", text="按！")),
                                    QuickReplyButton(action=MessageAction(label="按我", text="按！")),
                                    QuickReplyButton(action=MessageAction(label="按我", text="按！")),
                                    QuickReplyButton(action=MessageAction(label="按我", text="按！")),
                                    QuickReplyButton(action=MessageAction(label="按我", text="按！"))
-                               ]))
+                               ]))     
         line_bot_api.reply_message(event.reply_token, flex_message)
-    else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+
+        #for i in [5,4,3,2,1]:
+         #   line_bot_api.push_message(yourID, TextSendMessage(text= '倒數:'+str(i)))
+          #  time.sleep(1)
+    else:  
+        ##貼圖 
+        #sticker_message = StickerSendMessage(package_id=1,sticker_id=1)
+        #line_bot_api.reply_message(event.reply_token, sticker_message)
+        ##位置
+        #location_message  = LocationSendMessage(title = '推薦酒吧', address = '準備中',
+        #                                        latitude=24.155732100748097, longitude=120.64707590363155)
+        #line_bot_api.reply_message(event.reply_token, location_message)
+        ##圖片
+        #image_message = ImageSendMessage(
+         #   original_content_url='https://memeprod.ap-south-1.linodeobjects.com/user-template/e34fbd9d0e8fbd3b135283d9131fb51c.png',
+          #  preview_image_url= 'https://memeprod.ap-south-1.linodeobjects.com/user-template/e34fbd9d0e8fbd3b135283d9131fb51c.png')
+        #line_bot_api.reply_message(event.reply_token, image_message)
+        #line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage('test！'))
 #主程式
 import os
 if __name__ == "__main__":
